e5d4cb7a28971c815eae527b9594e944e3292e48
Andrew <fylhtqhecfrjd@gmail.com>
2024-11-07 22:28:37 +0300

    Some fixes

--- src/main/java/me/list_tw/clicktracker/controller/RedirectController.java
+++ src/main/java/me/list_tw/clicktracker/controller/RedirectController.java
@@ -2,22 +2,15 @@ package me.list_tw.clicktracker.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import me.list_tw.clicktracker.model.PathStatistics;
-import me.list_tw.clicktracker.repository.PathStatisticsRepository;
 import me.list_tw.clicktracker.service.ClickService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.List;
 
 @Controller
-@RequestMapping("/id")
 public class RedirectController {
 
     @Autowired
@@ -25,7 +18,7 @@ public class RedirectController {
 
     @GetMapping("/{promoId}")
     public void redirect(@PathVariable String promoId, HttpServletRequest request, HttpServletResponse response) {
-        String path = promoId;
+        String path = promoId; // Передаем promoId как путь
         clickService.recordClick(request, path);
         try {
             response.sendRedirect("https://leafcity.ru");
